83ad692970d2c3924bce38c076a5aa09f2e16a2e
idurnev <durnev87@gmail.com>
2022-06-27 15:41:42 +0900

    оплата v1

--- app/admin/forms.py
+++ app/admin/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, DateTimeField, IntegerField, TextAreaField, FileField, SelectField, \
     SelectMultipleField, MultipleFileField, BooleanField
-from wtforms.ext.sqlalchemy.fields import QuerySelectMultipleField, QuerySelectField
+# from wtforms.ext.sqlalchemy.fields import QuerySelectMultipleField, QuerySelectField
 from wtforms.validators import DataRequired, ValidationError
 from app.models import User, Group
 from app import Config
@@ -42,14 +42,14 @@ class CreateModerForm(FlaskForm):
     from app import create_app
     app = create_app(config_class=Config)
     with app.app_context():
-        group = QuerySelectMultipleField('Группа',
-                                         query_factory=Group.query.all,
-                                         get_pk=lambda group: group.id,
-                                         get_label=lambda group: group.name)
-        user = QuerySelectField('Пользователь',
-                                query_factory=User.query.filter(User.role == 'admin').all,
-                                get_pk=lambda user: user.tg_id,
-                                get_label=lambda user: user.first_name)
+        group = SelectMultipleField('Группа',
+                                    query_factory=Group.query.all,
+                                    get_pk=lambda group: group.id,
+                                    get_label=lambda group: group.name)
+        user = SelectField('Пользователь',
+                           query_factory=User.query.filter(User.role == 'admin').all,
+                           get_pk=lambda user: user.tg_id,
+                           get_label=lambda user: user.first_name)
         submit = SubmitField('Добавить')
 
 
